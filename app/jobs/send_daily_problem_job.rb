bbb207a60e5eafe00cb0bf279cc30fe7bf0877d8
JackMarx <seriousfools@gmail.com>
2017-08-04 14:10:32 -0500

    daily task

--- app/jobs/send_daily_problem_job.rb
+++ app/jobs/send_daily_problem_job.rb
@@ -7,12 +7,15 @@ class SendDailyProblemJob < ApplicationJob
     start_date = cohort.start_date
     today = Time.now.to_date
     time_diff = Time.diff(today, start_date)
-    daily_problem = DailyProblem.find_by(week: time_diff[:week], day: time_diff[:day])
+    if time_diff[:day] < 6
+      daily_problem = DailyProblem.find_by(week: time_diff[:week], day: time_diff[:day])
 
-    notifier = Slack::Notifier.new webhook do
-      defaults channel: "#math-test",
-               username: "Actualize Jack"
+      notifier = Slack::Notifier.new webhook do
+        defaults channel: "#math-test",
+                 username: "Actualize Jack"
+      end
+      notifier.ping daily_problem.content
     end
-    notifier.ping daily_problem.content
+    SendDailyProblemJob.set(wait_until: Date.tomorrow.noon).perform_later
   end
 end
