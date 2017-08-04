ff7e63ecca89ca31334323df24ed579c7195c0d1
JackMarx <seriousfools@gmail.com>
2017-08-04 18:25:34 -0500

    rake task

--- app/jobs/send_daily_problem_job.rb
+++ app/jobs/send_daily_problem_job.rb
@@ -2,20 +2,19 @@ class SendDailyProblemJob < ApplicationJob
   queue_as :default
 
   def perform
-    webhook = "https://hooks.slack.com/services/#{ENV['TEST_HOOK']}"
-    cohort = Cohort.find(1)
-    start_date = cohort.start_date
-    today = Time.now.to_date
-    time_diff = Time.diff(today, start_date)
-    if time_diff[:day] < 6
-      daily_problem = DailyProblem.find_by(week: time_diff[:week], day: time_diff[:day])
+    # webhook = "https://hooks.slack.com/services/#{ENV['TEST_HOOK']}"
+    # cohort = Cohort.find(1)
+    # start_date = cohort.start_date
+    # today = Time.now.to_date
+    # time_diff = Time.diff(today, start_date)
+    # if time_diff[:day] < 6
+    #   daily_problem = DailyProblem.find_by(week: time_diff[:week], day: time_diff[:day])
 
-      notifier = Slack::Notifier.new webhook do
-        defaults channel: "#math-test",
-                 username: "Actualize Jack"
-      end
-      notifier.ping daily_problem.content
-    end
-    SendDailyProblemJob.set(wait_until: Date.tomorrow.noon).perform_later
+    #   notifier = Slack::Notifier.new webhook do
+    #     defaults channel: "#math-test",
+    #              username: "Actualize Jack"
+    #   end
+    #   notifier.ping daily_problem.content
+    # end
   end
 end
---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,21 @@
+desc "To send out today's daily problem"
+task :daily_problem => :environment do
+  puts "sending daily problem"
+
+  webhook = "https://hooks.slack.com/services/#{ENV['TEST_HOOK']}"
+  cohort = Cohort.find(1)
+  start_date = cohort.start_date
+  today = Time.now.to_date
+  time_diff = Time.diff(today, start_date)
+  if time_diff[:day] < 6 && time_diff[:week] >= 0 && time_diff[:week] < 10
+    daily_problem = DailyProblem.find_by(week: time_diff[:week] + 1, day: time_diff[:day])
+
+    notifier = Slack::Notifier.new webhook do
+      defaults channel: "#math-test",
+               username: "Actualize Jack"
+    end
+    notifier.ping daily_problem.content
+  end
+
+  puts "done"
+end
